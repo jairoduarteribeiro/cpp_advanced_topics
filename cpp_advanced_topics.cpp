4f7136d758ee71cc5864326411b85e20536b80da
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-05-23 20:31:35 +0100

    create project

---
+++ cpp_advanced_topics.cpp
@@ -0,0 +1,12 @@
+﻿// cpp_advanced_topics.cpp : Defines the entry point for the application.
+//
+
+#include "cpp_advanced_topics.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
